59b18a0af8f97cbeac22064e24f97a7f7d697331
Ritik Kumar <ritik.kumar_cs18@gla.ac.in>
2019-09-20 21:36:27 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
 package com.company;
-
-
-
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args){
                 int a ;
                 String c ,d ,b;
